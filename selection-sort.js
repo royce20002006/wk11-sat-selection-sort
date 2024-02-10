f69a01a1449796f4a049f99c12a142badedc5597
Royce Calvin <royce20002006@yahoo.com>
2024-02-10 17:24:38 -0600

    finished

--- selection-sort.js
+++ selection-sort.js
@@ -33,25 +33,40 @@ function selectionSort(arr) {
 
 
 function selectionSortInPlace(arr) {
-
+debugger;
   // Set a pointer at zero diving the array into sorted and unsorted halves
-
+  let divider = 0;
   // Repeat while the unsorted half is not empty:
-
+  while(divider < arr.length) {
     // Do not move this console.log
     console.log(arr.join(","));
 
     // Find the index of the minimum value in the unsorted half
-
-    // Save the min value
-
+    let min = Infinity;
+    let minIndex;
+    for (let i = divider; i < arr.length; i++) {
+      if (arr[i] < min) {
+        // Save the min value
+        min = arr[i];
+        minIndex = i;
+      }
+    }
     // Shift every unsorted value to the left of the min value to the right by 1
-
+    arr.splice(divider,0,min);
+    minIndex = minIndex +  1
+    arr.splice(minIndex,1);
     // Put the min value at the divider
+    divider++
+    
+
 
     // Increment the divider and repeat
 
+  }
 }
+debugger;
+let newArr = [2,4,6,8,1,3,5,7,9];
+selectionSortInPlace(newArr)
 
 
 module.exports = [selectionSort, selectionSortInPlace];
